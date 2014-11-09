f0b038ea556e6c263e7719afdf6a1f35296f9536
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2014-11-09 23:25:30 +0300

    Add ElaborateCharts

--- wsgi/app.py
+++ wsgi/app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
-from stats import stats
-from worker import worker
+from .stats import stats
+from .worker import worker
+from elaboratecharts import ElaborateCharts
 
 app = Flask(__name__)
 app.register_blueprint(stats, url_prefix='/stats')
 app.register_blueprint(worker, url_prefix='/worker')
+charts = ElaborateCharts(app, url_prefix='/elaboratecharts')
 
 if __name__ == '__main__':
     app.run(debug=True)
